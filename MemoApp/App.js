f0ee779f4bf82faa362fa0406f3ad84f6d17ef9c
pl-pr-pm <pl.pr.pm.12@gmail.com>
2020-02-25 00:33:47 +0900

    Use react-native-stack

--- MemoApp/App.js
+++ MemoApp/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoLoginScreen from './src/screens/MemoLoginScreen';
 import MemoSignUpScreen from './src/screens/MemoSignUpScreen';
-import Appbar from './src/components/Appbar';
 
-export default class App extends React.Component {
-  render(){
-    return (
-      <View style={styles.container}>
-        <Appbar />
-       <MemoSignUpScreen/>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fffdf6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 10,
+
+const App = createStackNavigator({
+  Home: {
+    screen: MemoListScreen,
+  },
+}, {
+  navigationOptions: {
+    headerTitle: 'Memot'
+    , headerStyle:
+      { backgroundColor: '#265366' }
+    , headerTitleStyle:
+      { color: '#fff' }
   },
 });
+
+export default createAppContainer(App);
